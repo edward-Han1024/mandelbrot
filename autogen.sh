abbf180b4fd5583cb8d41f9a95d241344a5b6067
RandomPerson238 <babyed.han@gmail.com>
2026-01-03 13:59:42 -0700

    added more tracked files to improve building

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-libtoolize && aclocal && autoconf && automake
+libtoolize && aclocal && autoconf && automake --add-missing
 echo "Now run ./configure to configure the program"
 echo "You can view relevant options by running ./configure -h"
